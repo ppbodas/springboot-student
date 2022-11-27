9281a84a4670dcac40ce8d3087f23527484f18f6
Prathmesh Bodas <prathmb@amazon.com>
2022-11-26 17:01:55 -0800

    Initial Check in

---
+++ src/main/java/com/example/student/StudentApplication.java
@@ -0,0 +1,27 @@
+package com.example.student;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class StudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentApplication.class,
+				args);
+	}
+
+	@GetMapping
+	public List<String> hello() {
+		final ArrayList<String> strings = new ArrayList<>();
+		strings.add("Hello");
+		strings.add("World");
+
+		return strings;
+	}
+}
